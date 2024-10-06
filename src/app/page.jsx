00cfba111ccf1922fc2177da2f4c046dc89948e6
bananana0118 <bananana0118@gmail.com>
2024-10-06 14:10:01 +0900

    feat : test

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,28 +10,26 @@ export default function Home() {
   const [isFirst, setIsFirst] = useState(1); // isFirst 상태를 부모로 이동
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null); // Error state
+  const fetchWorkSpaceId = async () => {
+    if (workSpaceId !== null) return;
 
-  // useEffect(() => {
-  //   const fetchWorkSpaceId = async () => {
-  //     if (workSpaceId !== null) return;
-
-  //     try {
-  //       const res = await postWorkSpace();
-  //       if (res.status === 200) {
-  //         setWorkSpaceId(res.data);
-  //         setLoading(false); // Stop loading
-  //       } else {
-  //         throw new Error("Failed to get workspace ID");
-  //       }
-  //     } catch (err) {
-  //       console.error("refresh error", err);
-  //       setError("Failed to load workspace. Please try again.");
-  //       setLoading(false);
-  //     }
-  //   };
-
-  //   fetchWorkSpaceId();
-  // }, []);
+    try {
+      const res = await postWorkSpace();
+      if (res.status === 200) {
+        setWorkSpaceId(res.data);
+        setLoading(false); // Stop loading
+      } else {
+        throw new Error("Failed to get workspace ID");
+      }
+    } catch (err) {
+      console.error("refresh error", err);
+      setError("Failed to load workspace. Please try again.");
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchWorkSpaceId();
+  }, []);
 
   if (loading) return <div className="bg-white">adasd</div>;
   if (error) return <p>{error}</p>;
@@ -40,12 +38,11 @@ export default function Home() {
       {workSpaceId === null ? ( // null 체크
         <p>Loading workspace...</p>
       ) : (
-        <div className="bg-white">adasd</div>
-        // <ChatBot
-        //   spaceId={workSpaceId}
-        //   isFirst={isFirst}
-        //   setIsFirst={setIsFirst}
-        // />
+        <ChatBot
+          spaceId={workSpaceId}
+          isFirst={isFirst}
+          setIsFirst={setIsFirst}
+        />
       )}
     </>
   );
